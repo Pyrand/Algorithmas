7d8205c1541695b5c7c7dd9903f6eb40256c294f
Kerem Bürkük <128317497+Pyrand@users.noreply.github.com>
2024-05-16 21:24:27 +0300

    Add files via upload

--- Questions/8)KacaKadarAsal/8)KacaKadarAsal/main.c
+++ Questions/8)KacaKadarAsal/8)KacaKadarAsal/main.c
@@ -1,14 +1,38 @@
-//
-//  main.c
-//  8)KacaKadarAsal
-//
-//  Created by Kerem Bürkük on 14.05.2024.
-//
+//Girilen bir sayıya kadar kaç tane asal sayı olduğunu hesaplayan bir program yazın.
 
-#include <stdio.h>
+#include<stdio.h>
+#include<stdbool.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+bool asalKontrol(int sayi){
+    int i;
+    
+    for(i = 2; i < sayi; i++){
+        if(sayi % i == 0){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int asalSayım(int n){
+    int i, asalSayi = 0;
+    
+    for(i = 2; i < n; i++){
+        if(asalKontrol(i)){
+            asalSayi++;
+        }
+    }
+    
+    return asalSayi;
+}
+
+int main(){
+    
+    int n;
+    
+    printf("Kaca kadar asal sayi alinsin: ");
+    scanf("%d", &n);
+
+    printf("%d. sayıya kadar toplam %d asal sayi var.", n, asalSayım(n));
 }

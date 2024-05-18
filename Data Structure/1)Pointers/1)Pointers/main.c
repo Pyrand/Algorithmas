ac480908ba9a67683ff137d4f285fb496a913528
Kerem Bürkük <128317497+Pyrand@users.noreply.github.com>
2024-05-18 10:59:20 +0300

    Add files via upload

---
+++ Data Structure/1)Pointers/1)Pointers/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void add(int a, int b){
+    int c;
+    c = a+b;
+    printf("%d", c);
+    
+}
+
+int main() {
+    
+    int a, b;
+    a = 10;
+    b= 20;
+    add(a,b);
+   
+    
+    
+    
+}

e22376e507414b426a69cc8b05900718181f9004
Kerem Bürkük <128317497+Pyrand@users.noreply.github.com>
2024-05-18 10:46:17 +0300

    Final Hazirlik

--- Questions/Tekil Dizi.c
+++ Questions/Tekil Dizi.c
@@ -1,7 +1,6 @@
 //Girilen bir dizideki elemanlari, siralarini bozmadan ama tekil olarak yeni bir diziye aktaran bir program yazin. Ornegin girdi {3, 3, 87, 56, 1, 87, 3, 2 } ise cikti {3, 87, 56, 1, 2 } olmalidir.
 
 #include<stdio.h>
-#include<string.h>
 #include<stdbool.h>
 
 int main(){

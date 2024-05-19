ed9e308de4e6c25d7a33551e6a4b3c088c0fd9c5
Kerem Bürkük <128317497+Pyrand@users.noreply.github.com>
2024-05-19 14:43:11 +0300

    Delete Frequency.c

--- Frequency.c
+++
@@ -1,8 +0,0 @@
-//Forty students were asked to rate the quality of the food in the student cafeteria on a scale of 1 to 10 (1 means awful and 10 means excellent). This program places the 40 responses in an integer array and summarizes the results of the poll.
-
-#include<stdio.h>
-
-int main(){
-    
-    
-}

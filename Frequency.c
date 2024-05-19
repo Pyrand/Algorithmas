dd8f79f54028f2b0cbe92580972ea3caa8fda75f
Kerem Bürkük <128317497+Pyrand@users.noreply.github.com>
2024-05-19 14:42:36 +0300

    Final Hazırlık

---
+++ Frequency.c
@@ -0,0 +1,8 @@
+//Forty students were asked to rate the quality of the food in the student cafeteria on a scale of 1 to 10 (1 means awful and 10 means excellent). This program places the 40 responses in an integer array and summarizes the results of the poll.
+
+#include<stdio.h>
+
+int main(){
+    
+    
+}
